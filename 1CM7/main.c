4c88059c71ec4153e05bc2861583056871e9f16c
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-05-04 14:58:00 -0600

    if anidado, para formula general

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void){
 
-    int edad;
+    float a,b,c,discriminante,x1,x2;
 
-    printf("Ingresa tu edad ");
-    scanf("%d",&edad);
+    //1. Pide dato(s)
+    printf("Ingresa a ");
+    scanf("%f",&a);
+    printf("Ingresa b ");
+    scanf("%f",&b);
+    printf("Ingresa c ");
+    scanf("%f",&c);
 
-    if(edad < 18){
-        printf("NO pasas ='^(");
+    discriminante = pow(b,2)-4*a*c;
+
+    if(discriminante < 0){//Si la condicion es verdadera, no hagas 2  ni 3
+        printf("\nError! Raices imaginarias...\n");
     }
     else{
-        printf("Bienvenid@ =^)");
+        if(a==0){//Si la condicion es verdadera, no hagas 2  ni 3
+            printf("\nError! Raices indeterminadas...\n");
+        }
+        else{
+            //2. Calcula formula(s)
+            x1 = (-b+sqrt(pow(b,2)-4*a*c))/(2*a);
+            x2 = (-b-sqrt(pow(b,2)-4*a*c))/(2*a);
+            //3. Muestra resultado(s)
+            printf("\nx1 = %f\nx2 = %f\n",x1,x2);
+        }
     }
 
     return 0;
