c55fdefb37ebe57b8b4c9219179039d0cbb0dbb2
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-05-12 13:01:05 -0600

    Funciones para varios rangos

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -1,33 +1,89 @@
 #include <stdio.h>
 
+int enRangoParaIncisoA(int x);
+int enRangoParaIncisoB(int x);
+int enRangoParaIncisoC(int x);
+int enRangoParaIncisoD(int x);
+int fueraDeRangoParaIncisoE(int x);
+
 int main(void){
 
-    char situacion;
+    int n;
 
-    printf("Ingresa tu situacion ");
-    scanf("%c",&situacion);
+    printf("Ingresa tu numero ");
+    scanf("%d",&n);
 
-    if(situacion == 'R'){
-        printf("Situacion Regular");
-    }
-    else{
-        if(situacion == 'r'){
-            printf("Situacion regular");
-        }
-        else{
-            if(situacion == 'I'){
-                printf("Situacion Irregular");
-            }
-            else{
-                if(situacion == 'i'){
-                    printf("Situacion irregular");
-                }
-                else{
-                    printf("Situacion Invalida");
-                }
-            }
-        }
-    }
+    printf("%d\n",enRangoParaIncisoA(n));
+    printf("%d\n",enRangoParaIncisoB(n));
+    printf("%d\n",enRangoParaIncisoC(n));
+    printf("%d\n",enRangoParaIncisoD(n));
+    printf("%d\n",fueraDeRangoParaIncisoE(n));
 
     return 0;
+}
+
+int enRangoParaIncisoA(int x){
+    if(x<48){
+        return 0;
+    }
+    else{
+        if(x>57){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+}
+int enRangoParaIncisoB(int x){
+    if(x<65){
+        return 0;
+    }
+    else{
+        if(x>90){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+}
+int enRangoParaIncisoC(int x){
+    if(x<97){
+        return 0;
+    }
+    else{
+        if(x>122){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+}
+int enRangoParaIncisoD(int x){
+    if(enRangoParaIncisoB(x)){
+        return 1;
+    }
+    else{
+        if(enRangoParaIncisoC(x)){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
+int fueraDeRangoParaIncisoE(int x){
+    if(enRangoParaIncisoA(x)){
+        return 0;
+    }
+    else{
+        if(enRangoParaIncisoD(x)){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
 }
