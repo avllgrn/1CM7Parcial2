6e11021a29d855b8811da5dba95ff216a09783fd
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-05-23 12:23:46 -0600

    Contador [10, 1] con incrementos de 1

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -5,10 +5,10 @@ int main(void){
 
     int i;
 
-    i=50;
-    while( i >= 5 ){
+    i=10;
+    while( i >= 1 ){
         printf("%d ",i);
-        i = i-5;
+        i = i-1;
     }
 
     return 0;
