050cc9a6d3bb3950e4b7ac461653272d18b33df6
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-05-29 20:37:49 -0600

    2 Contadores ascendentes y contador descendente simultaneos, con for

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-    int i,j;
+    int i,j,k;
 
-    for(i=0,j=100; i<10 && j>0; i = i+1,j = j-10)
-        printf("i=%d j=%d\n",i,j);
+    for(i=0,j=5,k=100; k>0 ; i=i+1,j=j+5,k=k-10)
+        printf("i=%d j=%d k=%d\n",i,j,k);
 
     return 0;
 }
