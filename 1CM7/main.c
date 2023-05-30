ebb99baa66d8f3ee0107a38a23082a10b8730be2
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-05-29 20:28:31 -0600

    Muestra histograma de asteriscos

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -1,33 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "misFunciones.h"
-
-float f(float x);
 
 int main(void){
-    float x,ini,fin,paso;
-    int i;
+    int n,i;
+    printf("Ingresa n ");
+    scanf("%d",&n);
 
-    printf("Dame ini ");scanf("%f",&ini);
-    printf("Dame fin ");scanf("%f",&fin);
-    printf("Dame paso ");scanf("%f",&paso);
-    printf("\n\n");
-
-    printf("x\t\t| f(x) = sin(x)\n");
-    for(i=1;i<30;i=i+1)
-        printf("-");
-    printf("\n");
-
-    x = ini;
-    while(x<=fin){
-        printf("%f\t| %f\n",x,f(x));
-        x = x+paso;
+    printf("%d ",n);
+    i=1;
+    while(i<=n){
+        printf("*");
+        i = i+1;
     }
 
     return 0;
-}
-
-float f(float x){
-    return sin(convierteARadianes(x));
 }
