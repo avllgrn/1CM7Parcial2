1421dd4961da4e3b185237ee08bbadde73de8fb9
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-05-29 20:35:51 -0600

    Contador ascendente y contador descendente simultaneos, con for

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -3,13 +3,8 @@
 int main(void){
     int i,j;
 
-    i=0;
-    j=100;
-    while(i<10 && j>0){
+    for(i=0,j=100; i<10 && j>0; i = i+1,j = j-10)
         printf("i=%d j=%d\n",i,j);
-        i = i+1;
-        j = j-10;
-    }
 
     return 0;
 }
