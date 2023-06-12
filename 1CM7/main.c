a286bb46d050b97f80072813e8fb4839322d858f
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-12 12:57:43 -0600

    Serie de ln(1 + x)

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -4,16 +4,19 @@
 int main(void){
     int i, n;
     float x, s;
-    //-ln(1 - x), para |x| < 1
+    //ln(1 + x), para |x| < 1
 
     printf("Dame x ");scanf("%f",&x);
     printf("Dame n ");scanf("%d",&n);
 
     for(i=1, s=0; i<=n; i++){
-        printf("s=%f i=%d\n",s,i);
-        s = s+pow(x,i)/i;
+        printf("s=%f\ti=%d\n",s,i);
+        if(i%2 != 0)
+            s = s + pow(x,i)/i;
+        else
+            s = s - pow(x,i)/i;
     }
-    printf("\n\n-ln(1 - %f) = %f\n",x,s);
+    printf("\n\nln(1 + %f) = %f\n",x,s);
 
     return 0;
 }
