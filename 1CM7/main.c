f8d3b87b49efcd22771b90f4ba0cb12d483def8f
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-12 13:09:25 -0600

    Series del sen(x) y cos(x)

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
 #include <math.h>
 
+float convierteARadianes(float xGrad);
+long long factorial(int n);
+
 int main(void){
-    int i, n;
-    float x, s;
-    //ln(1 + x), para |x| < 1
+    int i, n, t;
+    float x, xRad, s;
 
     printf("Dame x ");scanf("%f",&x);
     printf("Dame n ");scanf("%d",&n);
+    xRad = convierteARadianes(x);
 
-    for(i=1, s=0; i<=n; i++){
-        printf("s=%f\ti=%d\n",s,i);
-        if(i%2 != 0)
-            s = s + pow(x,i)/i;
+    //La serie comienza con i=1
+    for(i=1, t=1, s=0; t<=n; t++, i = i+2){
+        //printf("s = %f\ti=%d\n",s,i);
+        if(t%2 != 0)
+            s = s + pow(xRad,i)/factorial(i);
         else
-            s = s - pow(x,i)/i;
+            s = s - pow(xRad,i)/factorial(i);
     }
-    printf("\n\nln(1 + %f) = %f\n",x,s);
+    printf("\n\nsen(%f) = %f\n\n",x,s);
 
+    //La serie comienza con i=0
+    for(i=0, t=1, s=0; t<=n; t++, i = i+2){
+        //printf("s = %f\ti=%d\n",s,i);
+        if(t%2 != 0)
+            s = s + pow(xRad,i)/factorial(i);
+        else
+            s = s - pow(xRad,i)/factorial(i);
+    }
+    printf("\n\ncos(%f) = %f\n\n",x,s);
     return 0;
+}
+
+float convierteARadianes(float xGrad){
+    return xGrad * M_PI / 180;
+}
+long long factorial(int n){
+    int i;
+    long long f=1;
+
+    for(i=2; i<=n; i++)
+        f *= i;
+
+    return f;
 }
